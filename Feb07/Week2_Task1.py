cbe07030c0caa1fa172538fff3a77aa31abe1929
Tahseen <tahseen123fatma@gmail.com>
2024-02-27 20:42:28 +0530

    WEEK2 Task

---
+++ Feb07/Week2_Task1.py
@@ -0,0 +1,25 @@
+# TASK1_WEEK2
+# Explain the difference between = Operator and == Operator in Python
+""" "=" Operator is an assignment operator which is used to assign the values to the variables
+whereas "==" operator is a comparison operator used to compare the values of variables."""
+a = 2  # Assignment Operator
+print(a)
+b = 2
+print(a == b)  # Comparison Operator. It returns boolean value
+
+# What does the ** Operator do in Python and how it is used?
+""" "*" Operator is an Arithmetic Exponentiation Operator meaning add as power to the values of variable .
+ eg.  2 ** 3 means 2 to the power 3. """
+x = 2
+y = 3
+print(int(2**3))  # Exponentiation Operator
+
+# What does ^ Operator do in Python, and in what context it commonly used?
+""" "^" Operator is a Bitwise XOR Operator in Python. It returns 1 if one of bit is 1. 
+This Operator is generally used to perform operations involving the individual bits of Integer."""
+c = 1
+d = 0
+print(c ^ d)  # Bitwise XOR
+e = 0
+f = 0
+print(e ^ f)  # Bitwise XOR
---
+++ Feb07/Week2_Task2.py
@@ -0,0 +1,29 @@
+# TASK2_WEEK2
+# Write a program to calculate the area of Circle given its radius  using formula area = pie * r * 2 (Take pie = 3.16)
+radius = float(input("Enter the radius of Circle : "))
+PIE = 3.14
+area = (PIE * radius * 2)
+print("Area of Circle is : ", area)
+
+# Create a program that takes two numbers as input
+# and prints whether the first number is greater than, less than, or equal to the second number.
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print("First number is equal to Second number", a == b)
+print("First number is less than Second number", a < b)
+print("First number is greater Second number", a > b)
+
+# Develop a Python script that calculates the square and cube of a given number.
+x = int(input("Enter the number :"))
+print("Square of given number :", x ** 2)
+print("Cube of given number :", x ** 3)
+
+
+
+
+
+
+
+
+
+
