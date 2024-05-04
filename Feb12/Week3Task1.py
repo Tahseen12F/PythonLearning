d92931c146ed0dbff83bca54e8f755a5cc832d25
Tahseen <tahseen123fatma@gmail.com>
2024-05-04 18:09:42 +0530

    Week3Task_1

--- Feb12/Week3Task1.py
+++ Feb12/Week3Task1.py
@@ -34,3 +34,24 @@ def reverse_number(name):
 print("Reverse of string", reverse_number(str))
 
 
+# Palindrome
+
+def reverse_string(str):
+    rev_str = ""
+    for c in str:
+        rev_str = c + rev_str
+    return rev_str
+
+
+original_str = input("Enter the String\n")
+original_str = original_str.lower()
+rev_str = reverse_string(original_str)
+print(rev_str)
+
+# Palindrome - str = rev_str -> p
+if original_str == rev_str:
+    print("Palindrome")
+else:
+    print("Not a Palindrome")
+
+
